54788e41961fefd657b9a7101d17c4672501947d
Cooper <dancersoftaranto@gmail.com>
2022-10-07 15:21:12 +0300

    css

--- src/helper/data.js
+++ src/helper/data.js
@@ -14,7 +14,7 @@ const data = [
     },
   },
   {
-    photographer: "ἐμμανυελ  ",
+    photographer: "Jesus",
     src: {
       large:
         "https://cdn.pixabay.com/photo/2017/02/07/16/47/kingfisher-2046453_960_720.jpg",
